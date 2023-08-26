358960ad7da55ea3f2582f8c45cb17b7423e5c10
Jiekun Liu <jiekunliu@Jiekuns-Air.ht.home>
2023-08-25 22:23:08 -0400

    basic function complete

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -17,6 +17,7 @@ export default function DashboardRootLayout({ children }) {
           <Sidebar />
           {children}
         </GlassPane>
+        <div id="modal"></div>
       </body>
     </html>
   );
